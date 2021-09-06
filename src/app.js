3bb22c0ab7ab8994deb9d07edba747af99290ab7
Luis Peixoto <luispeixoto@pq.uenf.br>
2021-09-06 18:20:46 -0300

    feat: messages routes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const dotenv = require('dotenv')
 const app = express()
 const auth = require('./routes/auth.routes')
 const user = require('./routes/user.routes')
+const messages = require('./routes/messages.routes')
 
 app.use(express.json())
 dotenv.config()
 
 app.use('/auth', auth)
 app.use('/user', user)
+app.use('/message', messages)
 
 module.exports = app
