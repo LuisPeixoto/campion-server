4e8af6e4a7294ad22023985b8c5a4694dec53e0c
yurigc77 <61841720+yurigc77@users.noreply.github.com>
2021-10-04 16:30:18 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,37 +2,37 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
-const userSchema = new Schema({
-  username: {
+const userSchema = new Schema({ //definindo schema
+  username: {  //apelido do usuario, precisa ser unico
     type: String,
     required: true,
     min: 3,
     unique: true
   },
 
-  name: {
+  name: { //nome do usuario
     type: String,
     required: true,
     min: 3
   },
 
-  password: {
+  password: { //senha
     type: String,
     required: true,
     min: 8
   },
 
-  avatar: {
+  avatar: { //url da foto do usuario
     type: String,
     default: ''
   },
 
-  followers: {
+  followers: { //vetor com o id dos seguidores
     type: Array,
     default: []
   },
 
-  followings: {
+  followings: { //vetor com o id dos seguidos
     type: Array,
     default: []
   }
