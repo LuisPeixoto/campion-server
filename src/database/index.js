0ca13c00f911a6a994d192c462411a2d2f45bad3
yurigc77 <61841720+yurigc77@users.noreply.github.com>
2021-10-04 16:55:40 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
 const URI = 'mongodb+srv://campion_luispeixoto:luispeixoto@campion-cluster.lsyp3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//Uri para acessar o banco (por favor não hackeiem)
 
-const connectDB = async () => {
-  await mongoose.connect(URI, {
-    useUnifiedTopology: true
+const connectDB = async () => { //função para  fazer a conexão com o banco
+  await mongoose.connect(URI, { //esperando conexão
+    useUnifiedTopology: true 
   }, () => {
-    console.log('Connected DB')
+    console.log('Connected DB') //confirmação de conexão
   })
 }
 
