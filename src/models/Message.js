5ec5524d3e066b93c0ef21e2f6c881c27def6038
yurigc77 <61841720+yurigc77@users.noreply.github.com>
2021-10-04 16:21:04 -0300

    Update Message.js

--- src/models/Message.js
+++ src/models/Message.js
@@ -2,23 +2,23 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
-const messageSchema = new Schema({
+const messageSchema = new Schema({ //definindo schema
   chatId: {
-    type: String
+    type: String //id da conversa a qual essa menssagem pertence
   },
 
   sender: {
-    type: String
+    type: String //id do usuario remetente
   },
   receiver: {
-    type: String
+    type: String //id do usuario destinatario
   },
   text: {
-    type: String
+    type: String //texto da menssagem
   }
 },
 {
-  timestamps: true
+  timestamps: true //horario da menssagem
 })
 
 module.exports = mongoose.model('message', messageSchema)
