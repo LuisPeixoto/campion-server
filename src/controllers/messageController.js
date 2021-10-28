a04d622a48f95e1bff021e3241297cbd3bed0cf0
Luis Fernando <luispeixoto@pq.uenf.br>
2021-10-28 14:29:43 -0300

    feat: implements benchmarks

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,10 +1,17 @@
 const Message = require('../models/Message')
+const LoremIpsum = require('lorem-ipsum').LoremIpsum
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
 
 const messageController = {
   async get (req, res) { // retorna o conteudo da mensagem atraves do id
     const { chatId } = req.params
     try {
+      const time1 = new Date().getUTCMilliseconds()
       const messages = await Message.find({ chatId }).sort({ createdAt: -1 })
+      const time2 = new Date().getUTCMilliseconds()
+      const result = time2 - time1
+
+      console.log(result)
 
       const data = messages.map((message) => {
         return {
@@ -28,12 +35,18 @@ const messageController = {
 
     try {
       const savedMessage = await newMessage.save()
+      const time1 = new Date().getUTCMilliseconds()
+      const TempoSavedMessage = await Message.aggregate([{ $collStats: { latencyStats: { histograms: true } } }])
+      const time2 = new Date().getUTCMilliseconds()
+      const result = time2 - time1
+
+      console.log(result)
 
       /// //////// parte do socket io /////
       const { io, connectedUsers } = req
       const { receiver, sender, text } = req.body
 
-      console.log(connectedUsers[receiver])
+      // console.log(connectedUsers[receiver])
 
       const userSocket = connectedUsers[receiver]
       if (userSocket) { // VERIFICAR SE O USUARIO ESTA ONLINE
@@ -49,6 +62,42 @@ const messageController = {
       }
 
       res.status(200).json(savedMessage)
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+  },
+  async sendMessage (req, res) {
+    const { sender, receiver, chatId } = req.params
+
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4
+      }
+    })
+
+    const data = {
+      chatId: chatId,
+      sender: sender,
+      receiver: receiver,
+      text: lorem.generateParagraphs(1)
+    }
+    const newMessage = new Message(data)
+
+    try {
+      // console.time('Tempo')
+      const time1 = Date.now()
+      const savedMessage = await newMessage.save()
+      const time2 = Date.now()
+      const result = time2 - time1
+      console.log(result)
+
+      res.status(200).json(result)
     } catch (err) {
       console.log(err)
       res.status(500).json(err)
--- src/routes/messages.routes.js
+++ src/routes/messages.routes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 
 router.get('/:chatId', messageController.get) // TODAS MENSAGENS DE UM CHAT DE ACORDO COM O ID
 router.post('/', messageController.newMessage) // CRIAR UMA NOVA MENSAGEM
+router.get('/:chatId/:sender/:receiver', messageController.sendMessage) // CRIAR UMA NOVA MENSAGEM
 
 module.exports = router
